a770129cc3014de1b5016578b9263e1a4afc0465
Pedro Barros <pedrobars7a@gmail.com>
2022-10-10 11:19:40 -0300

    Inserindo dados no sistema

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Moment } from 'src/app/Moment';
 
 @Component({
   selector: 'app-moment-form',
@@ -7,6 +8,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./moment-form.component.css'],
 })
 export class MomentFormComponent implements OnInit {
+  @Output() onSubmit = new EventEmitter<Moment>();
   @Input() btnText!: string;
 
   momentForm!: FormGroup;
@@ -25,8 +27,15 @@ export class MomentFormComponent implements OnInit {
   get title() {
     return this.momentForm.get('title')!;
   }
+
   get description() {
     return this.momentForm.get('description')!;
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+
+    this.momentForm.patchValue({ image: file });
   }
 
   submit() {
@@ -34,6 +43,8 @@ export class MomentFormComponent implements OnInit {
       return;
     }
 
-    console.log('Enviou formulário');
+    console.log(this.momentForm.value);
+
+    this.onSubmit.emit(this.momentForm.value);
   }
 }
